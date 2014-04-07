1ffa04f57b0ae3f837914dc66a5167b58c457697
Prelang Builder <builder@prelang.com>
2014-04-07 22:16:55 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrueLawn::Application.config.secret_key_base = 'b6a127fc4f43f4fdb957c1a6f02b38626d0926131968dc29b3e228525015c123b2efc30de6cfeecfcf13226ac85a0380d7d2ced8c1ee221ba2b7a61a09e2e4af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrueLawn::Application.config.session_store :cookie_store, key: '_true_lawn_session'
